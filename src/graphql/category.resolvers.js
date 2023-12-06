302383f1ed369e1cc35b86bb04312c39d57c4836
Rafael Osorio <90006211+rafaelosorioa@users.noreply.github.com>
2023-12-06 13:02:46 -0500

    Class 19: JWT validation

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,7 +1,10 @@
+const boom = require('@hapi/boom');
 const CategoryService = require('./../services/category.service');
 const categoryService = new CategoryService();
 
-const addCategory = async (_, { dto }) => {
+const addCategory = async (_, { dto }, context) => {
+  const { user } = await context.authenticate('jwt', { session: false });
+  if (!user) throw boom.unauthorized('jwt is not valid');
   const category = await categoryService.create(dto);
   return category;
 };
